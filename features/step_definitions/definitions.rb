f3b74dd3cceabb2ad684ebf8df4941e824ceb1d0
yung76 <bmr.www@gmail.com>
2019-02-07 12:01:32 -0300

    Add locator

--- features/step_definitions/definitions.rb
+++ features/step_definitions/definitions.rb
@@ -1,8 +1,10 @@
-
-
 Given /^browser to web site "([^"]*)"$/ do |url|
-    #browser
     Capybara.app_host = url
     visit ('/')
 end
 
+Then /^search a jobs$/ do 
+    puts 'Looking for kind jobs'
+    find(:xpath, "//div[@class='Table-column Table-headline Table-middle is-wrapped is-firstMobile']//div[contains (.,'Aseguramiento de la calidad, localización y servicio al cliente')]").click
+    sleep 5
+end 
